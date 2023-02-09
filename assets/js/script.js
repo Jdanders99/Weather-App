5634f56cf8fe3597a912bb619e58b196cd327554
Jakob A <jdanders1999@gmail.com>
2023-02-09 14:24:09 -0500

    Added function to get data from recent searches in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,19 @@ function convertSearch() {
         })
 }
 
+function convertHistorySearch(historyBtnInfo) {
+    var input = historyBtnInfo.textContent;
+
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=1&appid=b539f961ee018c36b88d3838ba7bcfc2")
+        .then((response) => response.json())
+        .then(function (data) {
+            var lon = data[0].lon;
+            var lat = data[0].lat;
+            var city = data[0].name;
+            getWeatherData(lon, lat, city);
+        })
+};
+
 function getWeatherData(lon, lat, city) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely&units=imperial&appid=3235f6ca43f152b21beee3053909231f")
         .then((response) => response.json())
